b7adde2570d1ef4c9696a960df3a9e6ac24155a7
Ralf Westphal <info@ralfw.de>
2017-11-19 13:47:39 +0100

    added description

--- src/appcfg/Properties/AssemblyInfo.cs
+++ src/appcfg/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("appcfg")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription(".NET application configuration with command line parameters, config file, and environment variables made easy.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("appcfg")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2017 by Ralf Westphal")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: InternalsVisibleTo("appcfg_tests")]
